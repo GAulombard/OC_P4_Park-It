5a47b9f2743457a0ba21c5a88f0208815defd238
Geoffrey Aulombard <geoffrey.aulombard@gmail.com>
2021-04-08 12:32:55 +0200

    added FindBugs maven plugin in pom

---
+++ src/test/java/com/parkit/parkingsystem/TicketDAOTest.java
@@ -0,0 +1,26 @@
+package com.parkit.parkingsystem;
+
+import org.junit.jupiter.api.Test;
+
+public class TicketDAOTest {
+
+    @Test
+    public void saveTicketTest() {
+        //GIVEN
+
+        //WHEN
+
+        //THEN
+    }
+
+    @Test
+    public void getTicketTest() {
+
+    }
+
+    @Test
+    public void updateTicketTest() {
+
+    }
+
+}
