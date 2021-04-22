38c91c2ce6832d5d98b6826483d5c6d85fb0e800
Geoffrey Aulombard <geoffrey.aulombard@gmail.com>
2021-04-22 14:48:33 +0200

    fixed all bugs from SpotBugs

--- src/main/java/com/parkit/parkingsystem/config/DataBaseConfig.java
+++ src/main/java/com/parkit/parkingsystem/config/DataBaseConfig.java
@@ -3,17 +3,33 @@ package com.parkit.parkingsystem.config;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.sql.*;
 
 public class DataBaseConfig {
 
     private static final Logger logger = LogManager.getLogger("DataBaseConfig");
+    private String line;
 
     public Connection getConnection() throws ClassNotFoundException, SQLException {
         logger.info("Create DB connection");
         Class.forName("com.mysql.cj.jdbc.Driver");
+        try {
+            File file = new File(".idea/jdbcpassword.txt");
+            InputStreamReader inputStreamReader = new InputStreamReader(new FileInputStream(file),StandardCharsets.UTF_8);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            line = bufferedReader.readLine();
+            bufferedReader.close();
+            inputStreamReader.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/prod?serverTimezone=UTC","root","rootroot");
+                "jdbc:mysql://localhost:3306/prod?serverTimezone=UTC","root",line);
     }
 
     public void closeConnection(Connection con){
